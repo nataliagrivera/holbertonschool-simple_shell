5f58be01ef7f5bfcd145ceea700b146d2a0a6f9b
nataliagrivera <nataliagrivera62@gmail.com>
2023-08-15 20:20:27 +0000

    Adding drafted header file, and main shell file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#endif
---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int main( int ac, char **av[], char **env)
+{
+
+
+while (TRUE)
+	printf("$ ");
+	getline(buffer, size, stdin)
+
+
+}
