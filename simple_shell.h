b30f7ce83cd905b8eab42a9cace620557a6e4098
Joshua7792 <j.santiago39@hotmail.com>
2023-08-17 10:59:08 -0400

    fixed main.h

--- simple_shell.h
+++ simple_shell.h
@@ -5,8 +5,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-#include<sys/wait.h>
-#include<unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int ac, char **av, char **env);
 void execute_command(char *command, char **env);
