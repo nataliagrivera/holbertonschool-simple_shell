0701351ec651521eb589632451039560fc032a36
Joshua7792 <j.santiago39@hotmail.com>
2023-08-23 23:36:22 -0400

    non-interactive mode

--- main.c
+++ main.c
@@ -51,7 +51,6 @@ int main(int ac, char **av, char **env)
 	return (0); /* Exit the shell */
 }
 
-
 #include "simple_shell.h"
 /**
  * handle_non_interactive_mode - Handles the non-interactive mode of the shell
@@ -65,28 +64,38 @@ int main(int ac, char **av, char **env)
  */
 void handle_non_interactive_mode(char **env)
 {
-	char *line = NULL; /* Stores the input line */
-	size_t len = 0;	   /* Initial size for getline() */
-	ssize_t read;	   /* Stores the number of characters read */
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int status;
+	pid_t child_pid = fork();
 
-	/* Loop to read and process input lines in non-interactive mode */
 	while ((read = getline(&line, &len, stdin)) != -1)
 	{
-		/* Remove newline character from input line if present */
 		if (read > 0 && line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		/* Check if the user wants to exit the shell */
 		if (strcmp(line, "exit") == 0)
 			break;
 
-		/* Skip processing if the input line is empty or starts with a space */
 		if (line[0] == '\0' || line[0] == ' ')
 			continue;
 
-		/* Execute the command specified in the input line */
-		execute_command(line, env);
-	}
+		if (child_pid == -1)
+		{
+			perror("fork");
+			return;
+		}
 
-	free(line); /* Free the allocated memory for the input line */
+		if (child_pid == 0)
+		{
+			execute_command(line, env);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			waitpid(child_pid, &status, 0);
+		}
+	}
+	free(line);
 }
