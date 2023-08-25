41cc5d7104dc0499d7777bdb07a1d2481cb9680f
Joshua7792 <j.santiago39@hotmail.com>
2023-08-25 03:08:07 -0400

    delete get_path function, no needed

--- simple_shell.h
+++ simple_shell.h
@@ -12,7 +12,6 @@
 
 int main(int ac, char **av, char **env);
 void execute_command(char *command, char **env);
-char **get_paths(void);
 void create_child_process(char **tokens, char **env);
 void free_token_array(char **tokens, int token_count);
 void free_paths_array(char **paths, int num_paths);
